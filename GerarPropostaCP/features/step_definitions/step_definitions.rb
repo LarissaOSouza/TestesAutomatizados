7cfe20349d97c22437f13aeadba68a881ee666c9
LarissaOSouza <larissa.souza@finanzero.com.br>
2018-07-23 11:26:42 -0300

    cleber

--- GerarPropostaCP/features/step_definitions/step_definitions.rb
+++ GerarPropostaCP/features/step_definitions/step_definitions.rb
@@ -2,83 +2,91 @@
 Given("Eu estou na home e Clico em emprestimos") do
   visit('https://beta.finanzero.com.br/')
   page.find(:id,"CP").click
+  @page
 end
   
 When("Start to type your When step here estou na página de Emprestimo prencho meus dados") do
+  ## COPACABANA ##
   page.should have_content('Empréstimo Pessoal 100% Online')
   #Periodo do empréstimo    
   select('18', :from => 'loanPeriodInMonths')
   #Dados Pessoais Página 1
-  fill_in('fullName', :with => 'Sebastião Cauã Vieira')
+  fill_in('fullName', :with => 'Teste Automatizado')
   fill_in('telephoneNumber', :with => '11944979831')
   # ALTERAR EMAIL
-  fill_in('email', :with => 'testeautomatizado@30622447041.com.br')
+  fill_in('email', :with => 'testefinanzero4@06520442046.com')
   select('Funcionário Público', :from => 'workStatus')
   select('14', :from => 'DD_birthDate')
   select('7', :from => 'MM_birthDate')
   select('1985', :from => 'YY_birthDate')
   select('Solteiro(a)', :from => 'civilStatus')
   # ALTERAR CPF
-  fill_in('personalNumber', :with => '30622447041')
+  fill_in('personalNumber', :with => '06520442046 ')
   page.find(:id,'buttonCompare').click 
   #PÁGINA 2
-  #NÃO POSSUI RESTRIÇÃO DE CRÉDITO
-  sleep(10)
-  page.find(:xpath,'//*[@id="container"]/div/div/div/div[1]/form/div[1]/div/div[1]/div[2]/div/label[1]').click    
-  fill_in('monthlyIncome', :with => '4500')
+  #POSSUI RESTRIÇÃO DE CRÉDITO ? NÃO
+  page.find(:xpath,'//*[@id="hasCreditRestrictions"]/button[2]').click    
+  #QUANTO VOCÊ GANHA POR MÊS ?
+  fill_in('monthlyIncome', :with => '3000')
+  #BANCO E TIPO DE CONTA
   select('Bradesco', :from => 'bankName')
-  select('Conta corrente comum', :from => 'accountType')
+  select('Conta com cheque especial', :from => 'accountType')
+  #MOTIVO DO EMPRESTIMO
+  select('Financiar estudos', :from => 'loanReason')
+  #GÊNERO FEMININO
+  page.find(:xpath,'//*[@id="gender"]/button[2]').click    
+  #Possui carro quitado no seu nome ? Não
+  page.find(:xpath,'//*[@id="ownsVehicle"]/button[2]').click 
+  #VOCÊ ESTUDOU ATÉ?
+  sleep(5)
+  select('Superior completo', :from => 'education') 
+  #CEP
   fill_in('zipCode', :with => '04658280')
-  sleep(5)
-  page.find(:xpath,'//*[@id="container"]/div/div/div/div[1]/form/div[1]/div/div[7]/div[2]/div/label[1]').click    
-  sleep(5)
-  page.find(:xpath,' //*[@id="container"]/div/div/div/div[1]/form/div[5]/button').click    
+   #BOTÃO SALVAR/PROXIMA PÁGINA
+  page.find(:xpath,'//*[@id="root"]/div/div[2]/div/div[2]/div/div/form/div[2]').click    
+  page.find(:id,"btn-with-message").click
   #PÁGINA 3 
+  #TIPO DO DOCUMENTO
   sleep(10)
-  select('Própria', :from => 'housing')
-  select('Mais de dois anos', :from => 'housingTime')
-  fill_in('streetNumber', :with => '1151')
-  select('Feminino', :from => 'gender')
-  select('Superior completo', :from => 'education')
   select('RG', :from => 'RGDocType')
+  #NÚMERO DO DOCUMENTO ESCOLHIDO/ORGÃO EMISSOR/ ESTADO/ DATA DE EMISSÃO
   fill_in('RG', :with => '384127794')
   select('SSP', :from => 'RGExpeditorOrganisation')
-  select('SP', :from => 'RGOriginState')
   find("select[class=day]").select("10")
   find("select[class=month]").select("10")
   find("select[class=year]").select("2010")
+  #NACIONALIDADE/ ESTADO ONDE NASCEU
+  select('Brasileira', :from => 'nationality')
   select('SP', :from => 'stateOfBirth')
   fill_in('placeOfBirth', :with => 'São Paulo')
-  fill_in('mothersName', :with => 'Automatizado Mãe')
-  page.find(:xpath,'//*[@id="container"]/div/div/div/div[1]/form/div/div[3]/button').click   
-  page.find(:xpath,'//*[@id="container"]/div/div/div/div[1]/form/div/div[3]/button').click 
-  #PÁGINA 4 
-  sleep(10)
-  select('Financiar estudos', :from => 'loanReason')
+  #DADOS DA RESIDÊNCIA
+  #NÚMERO
+  fill_in('streetNumber', :with => '1151')
+  #VOCÊ MORA NESTE LUGAR HÁ ?
+  select('Mais de dois anos', :from => 'housingTime')
+  # E É UMA CASA ?
+  select('Própria', :from => 'housing')
+  sleep(5)
+  #BOTÃO SALVAR/PROXIMA PÁGINA
+  page.find(:xpath,'//*[@id="btn-with-message"]').click 
+  ## PÁGINA 4 ##
+  #DADOS DA RENDA 
   select('Holerite', :from => 'incomeProofDocument')
+  select('05', :from => 'incomePaymentDay')
   fill_in('companyName', :with => 'FinanZero')
   select('Administrador de Empresas', :from => 'jobPosition')
-  find("select[class=day]").select("10")
-  find("select[class=month]").select("10")
-  find("select[class=year]").select("2001")
-  select('05', :from => 'incomePaymentDay')
-  select('Crédito em conta', :from => 'incomePaymentMethod')
-  page.find(:xpath,'//*[@id="container"]/div/div/div/div[1]/form/div/div[3]/button').click   
-  #PÁGINA 5
-  sleep(10)
-  fill_in('agency', :with => '0022')
-  fill_in('accountNumber_acc', :with => '11111')
-  find("input[class=accdig]").set("3")
-  find("select[class=month]").select("1")
-  find("select[class=year]").select("2001")
-  page.find(:xpath,'//*[@id="container"]/div/div/div/div[1]/form/div/div[2]/button').click
+  select('7', :from => 'MM_employmentDate')
+  select('2009', :from => 'YY_employmentDate')
+  #DADOS BANCÁRIOS
+  fill_in('agency', :with => '2491')
+  fill_in('accountNumber', :with => '0760174')
+  fill_in('accountNumber-digit', :with => '3')
+  select('10', :from => 'DD_accountOpenDate')
+  select('7', :from => 'MM_accountOpenDate')
+  select('2005', :from => 'YY_accountOpenDate')
+  #BOTÃO SALVAR/PROXIMA PÁGINA
+  page.find(:xpath,'//*[@id="btn-with-message"]').click   
 end
-  
 Then("Start to type your Then step here Gerei uma proposta") do
-  page.should have_content('Consultando parceiros')
+  page.should have_content('Preenchimento Completo')
 end  
-  #select('Autônomo', :from => 'workStatus')
-  #select('Empresário ou PJ', :from => 'workStatus')
-  #select('Profissional Liberal', :from => 'workStatus')
-  #select('Aposentado ou Pensionista', :from => 'workStatus')
-  # select('Funcionário do Setor Privado', :from => 'workStatus')
